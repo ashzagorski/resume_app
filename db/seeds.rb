97fce0ed341e7b95b8acf9243c1e6a1036c5e23a
Nakamal <nickhaugland@gmail.com>
2018-10-30 20:42:29 -0500

    now Jake is added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.create!([
+  {first_name: "Jake", last_name: "Jortals", email: "jjortals@hotmail.com", phone_number: "(555)555-5555", bio: "I hail from Jacksonville,FL, so yes I have always been this way because I'm from Jacksonville", linkedIn_URL: "Jortals", twitter_handle: "jortals jokes by jake", personal_website_URL: "www.dave.uk", online_resume_URL: "www.dave.uk/resume", github_URL: "www.github.com/jortalsportals", photo: nil}
+])
