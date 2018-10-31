989f9f50687d6ec535e510eeee33a449cb997d6f
Nakamal <nickhaugland@gmail.com>
2018-10-30 20:37:35 -0500

    added a fake student (welcome Jake), student model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  namespace :api do
-  get '/students' => 'student#index'
+  get '/students' => 'students#index'
   post '/students' => 'students#create' 
+  get '/students/:id' => 'students#show' 
+  patch '/students/:id' => 'students#update'
+  delete '/students/:id' => 'students#destroy' 
  end
 end
