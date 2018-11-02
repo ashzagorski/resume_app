b34fd73253673f6220c5ccad9b2720ad2e4c4898
Thema Peters <tpete6957@gmail.com>
2018-11-01 21:00:50 -0500

    fixes

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
   has_many :skills
 
    validates :first_name, presence: true
-   validates :last_name presence: true
+   validates :last_name, presence: true
    validates :email, presence: true, uniqueness: true
   
 end
