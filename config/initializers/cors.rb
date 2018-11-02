c7c878b071ccf449aab62f4369ec7f35381cc70e
Thema Peters <tpete6957@gmail.com>
2018-11-01 21:18:40 -0500

    whitelist

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://blooming-caverns-85738.herokuapp.com'
+    origins 'localhost:8080', 'https://blooming-caverns-85738.herokuapp.com', 'https://suspicious-northcutt-40b17d.netlify.com'
 
     resource '*',
       headers: :any,
