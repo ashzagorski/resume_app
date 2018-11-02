bf20c09d9c0767eb7ec6cd896d0f6c24cf1876d5
Thema Peters <tpete6957@gmail.com>
2018-11-01 20:38:52 -0500

    sessions update

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,9 @@ class Student < ApplicationRecord
   has_many :educations
   has_many :experiences
   has_many :skills
+
+   validates :first_name, presence: true
+   validates :last_name presence: true
+   validates :email, presence: true, uniqueness: true
   
 end
